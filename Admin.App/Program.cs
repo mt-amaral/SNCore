e80a1374494c27a393e8bad647e9c775ef8baf0e
Mateus Amaral <amaral__@outlook.com>
2024-06-23 19:29:26 -0300

    MudBlazor instalado.

--- Admin.App/Program.cs
+++ Admin.App/Program.cs
@@ -2,10 +2,13 @@ using Admin.App;
 using Admin.App.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddMudServices();
+
 builder.Services.AddScoped<HardwareApi>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:8081/") });
