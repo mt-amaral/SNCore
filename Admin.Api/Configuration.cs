b9847d485f05a9f77b025a7e35109f9c09ee813a
Mateus <yoda@yodacode.net>
2024-06-27 12:05:08 -0300

    Finalização de Migração e justes em Cors.

--- Admin.Api/Configuration.cs
+++ Admin.Api/Configuration.cs
@@ -6,7 +6,7 @@ public class Configuration
 
     public static string AdminApiUrl = "https://localhost:8081";
 
-    public static string AdminAppUrl = "http://localhost:5056";
+    public static string AdminAppUrl = "http://localhost:8091";
 
     public static string AdminApiConteiner = "172.19.254.2";
 
