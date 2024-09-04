5c816d9e67604b2d6aaeba0c6158585e4fe5fd4f
Mateus Amaral <amaral__@outlook.com>
2024-09-03 22:25:26 -0300

    Refinamento dependecyInjection.

--- Admin.Infrustructure/DependecyInjection.cs
+++ Admin.Infrustructure/DependecyInjection.cs
@@ -22,26 +22,12 @@ public static class DependecyInjection
             options.UseSqlServer(configuration.GetConnectionString("ConnectionDev"),
                 m => m.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName));
         });
-        // Services
-        services.AddScoped<IHardwareService, HardwareService>();
-        services.AddScoped<ISnmpService, SnmpService>();
-        services.AddScoped<ITelnetService, TelnetService>();
 
+        services.AddApplicationServices();
 
-        // Repositories
-        services.AddScoped<IHardwareRepository, HardwareRepository>();
-        services.AddScoped<ISnmpRepository, SnmpRepository>();
-        services.AddScoped<ITelnetRepository, TelnetRepository>();
+        services.AddRepositories();
 
-        // Validation
-        services.AddScoped<IValidator<HardwareRequest>, HardwareValidation<HardwareRequest>>();
-        services.AddScoped<IValidator<HardwarePayload>, HardwareValidation<HardwarePayload>>();
-
-        services.AddScoped<IValidator<SnmpRequest>, SnmpValidation<SnmpRequest>>();
-        services.AddScoped<IValidator<SnmpPayload>, SnmpValidation<SnmpPayload>>();
-
-        services.AddScoped<IValidator<TelnetRequest>, TelnetValidation<TelnetRequest>>();
-        services.AddScoped<IValidator<TelnetPayload>, TelnetValidation<TelnetPayload>>();
+        services.AddValidators();
 
         return services;
     }
