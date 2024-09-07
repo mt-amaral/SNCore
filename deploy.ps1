995751626580b016522576a78d6b8be0a69d8350
Mateus Amaral <amaral__@outlook.com>
2024-09-07 16:16:31 -0300

    Removendo api de controle de conexão.

--- deploy.ps1
+++ deploy.ps1
@@ -1,5 +1,4 @@
 docker build -t api_admin:1.0.0 --target api -f Admin.Api/Dockerfile .;
-docker build -t api_connection_control:1.0.0 --target api_connection -f ConnectionControl.Api/Dockerfile .;
 docker build -t app_admin:1.0.0 --target app -f Admin.App/Dockerfile .;
 docker build -t sql_server:1.0.0 --target sql -f Admin.Sql.Test/Dockerfile .;
 
