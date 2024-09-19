dc45b231a49d89340f92727db0d39c565f9076c7
Mateus <mateus.freitas@fikdigital.com.br>
2024-09-19 17:45:25 -0300

    Removendo dbset manual.

--- Admin.Persistence/Context/ApplicationDbContext.cs
+++ Admin.Persistence/Context/ApplicationDbContext.cs
@@ -7,12 +7,6 @@ public class ApplicationDbContext : DbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
-    public DbSet<Host> Host { get; set; }
-    public DbSet<Snmp> Snmp { get; set; }
-    public DbSet<Telnet> Telnet { get; set; }
-    public DbSet<HostGroup> HostGroup { get; set; }
-    public DbSet<HostModel> HostModel { get; set; }
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
