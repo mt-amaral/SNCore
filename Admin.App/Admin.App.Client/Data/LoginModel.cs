16219b36336140f15554d03b68a983b464a9e69a
Mateus Amaral <amaral__@outlook.com>
2025-02-02 00:45:14 -0300

    Adicionando login.

---
+++ Admin.App/Admin.App.Client/Data/LoginModel.cs
@@ -0,0 +1,8 @@
+namespace Admin.App.Client.Data;
+
+public class LoginModel
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public bool RememberMe { get; set; }
+}
