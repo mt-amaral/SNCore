4e19217ec2d9c607489649c3ccad3fb0b6e042e8
Mateus Amaral <amaral__@outlook.com>
2025-02-03 23:06:27 -0300

    Refinamento variaveis e retornos async.

--- Admin.App/Admin.App.Client/Data/LoginModel.cs
+++ Admin.App/Admin.App.Client/Data/LoginModel.cs
@@ -2,7 +2,7 @@ namespace Admin.App.Client.Data;
 
 public class LoginModel
 {
-    public string Email { get; set; }
-    public string Password { get; set; }
+    public string Email { get; set; } = String.Empty;
+    public string Password { get; set; } = String.Empty;
     public bool RememberMe { get; set; }
 }
